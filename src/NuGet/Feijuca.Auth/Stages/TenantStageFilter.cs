30568c54453c605f5f2c70751151e6eec1e958dc
Guilherme Giannini <guilherme.giannini@smartinnovate.com.br>
2025-11-06 10:23:48 -0300

    Realocando corretamente classe

--- src/NuGet/Feijuca.Auth/Stages/TenantStageFilter.cs
+++ src/NuGet/Feijuca.Auth/Stages/TenantStageFilter.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.RegularExpressions;
 
-public class Class1
+namespace Feijuca.Auth.Stages;
+
+public static class TenantStageFilter
 {
-	public Class1()
-	{
-	}
+    public static FilterDefinition<BsonDocument> MatchByTenants(IEnumerable<string> tenantNames)
+    {
+        var names = tenantNames.Select(x => new BsonRegularExpression(new Regex(x, RegexOptions.IgnoreCase)));
+
+        var filter = new BsonDocument(
+            "Tenants",
+            new BsonDocument("$in", BsonArray.Create(names)));
+
+        return new BsonDocumentFilterDefinition<BsonDocument>(filter);
+    }
+
+    public static FilterDefinition<BsonDocument> MatchByTenant(string tenant)
+    {
+        var filter = new BsonDocument(
+            "Tenant",
+            new BsonDocument("$eq", tenant));
+
+        return new BsonDocumentFilterDefinition<BsonDocument>(filter);
+    }
 }
