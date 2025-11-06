f3b740421e47db964f9cfadb0d67e6182234bf08
Felipe Mattioli <felipe.mattioli@smartinnovate.com.br>
2025-11-06 15:14:30 +0100

    FEAT: forcing ppipeine

--- src/NuGet/Feijuca.Auth/Http/Client/IFeijucaAuthClient.cs
+++ src/NuGet/Feijuca.Auth/Http/Client/IFeijucaAuthClient.cs
@@ -2,12 +2,11 @@
 using Mattioli.Configurations.Models;
 using Feijuca.Auth.Http.Responses;
 
-namespace Feijuca.Auth.Http.Client
+namespace Feijuca.Auth.Http.Client;
+
+public interface IFeijucaAuthClient
 {
-    public interface IFeijucaAuthClient
-    {
-        Task<Result<TokenDetailsResponse>> AuthenticateUserAsync(string username, string password, CancellationToken cancellationToken);
-        Task<Result<PagedResult<UserResponse>>> GetUsersAsync(int maxUsers, string jwtToken, CancellationToken cancellationToken);
-        Task<Result<UserResponse>> GetUserAsync(string userame, string jwtToken, CancellationToken cancellationToken);
-    }
+    Task<Result<TokenDetailsResponse>> AuthenticateUserAsync(string username, string password, CancellationToken cancellationToken);
+    Task<Result<PagedResult<UserResponse>>> GetUsersAsync(int maxUsers, string jwtToken, CancellationToken cancellationToken);
+    Task<Result<UserResponse>> GetUserAsync(string userame, string jwtToken, CancellationToken cancellationToken);
 }
